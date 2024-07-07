5a0a9136637d43d7ceef2f92fbee0b84b20ade5f
Neeraja Maria Noel <130915823+neerajamarianoel@users.noreply.github.com>
2024-07-07 17:01:50 +0530

    Update retail_order_sql.sql

--- retail_order_sql.sql
+++ retail_order_sql.sql
@@ -6,17 +6,6 @@ group by product_id
 order by revenue desc
 limit 5;
 
--- Find the top 5 selling product in each region
-with cte as(
-select region,product_id,sum(sale_price) as revenue
-from orders
-group by region,product_id)
-select * from (
-select *
-,row_number() over (partition by region order by revenue desc) as rn
-from cte) A
-where rn <=5
-
 -- Which state has made the highest average profit?
 select state,category,avg(profit* quantity) as avg_profit
 from orders
